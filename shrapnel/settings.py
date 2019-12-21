7af1582ec2a5e196dd5a69fdad10229261f04f23
Ömer Faruk Abacı <omerfabaci@gmail.com>
2019-12-22 02:40:58 +0300

    Configure heroku and database settings

--- shrapnel/settings.py
+++ shrapnel/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -86,14 +87,7 @@ WSGI_APPLICATION = 'shrapnel.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'shrapneldb',
-        'USER': 'shrapnel',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',
-        'PORT': '5433',
-    }
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 
