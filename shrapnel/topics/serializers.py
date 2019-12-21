97209dd0aedcd8746bf55eabb77f48fa5011ae6f
Ömer Faruk Abacı <omerfabaci@gmail.com>
2019-12-21 19:15:31 +0300

    Create TopicSerializer skeleton

---
+++ shrapnel/topics/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from shrapnel.topics.models import Topic
+from shrapnel.users.models import User
+
+
+class TopicSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.filter(is_deleted=False), required=True)
+
+    class Meta:
+        model = Topic
+        fields = (
+            "title",
+            "user",
+            "date_created",
+            "date_updated"
+        )
