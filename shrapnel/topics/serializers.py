a578acd836cec81064aaebc029b106105e8f7658
Ömer Faruk Abacı <omerfabaci@gmail.com>
2019-12-21 21:19:33 +0300

    Create EntrySerializer, add entries field to TopicSerializer

--- shrapnel/topics/serializers.py
+++ shrapnel/topics/serializers.py
@@ -1,11 +1,27 @@
+from drf_extra_fields.relations import PresentablePrimaryKeyRelatedField
+
 from rest_framework import serializers
 
 from shrapnel.topics.models import Topic
 from shrapnel.users.models import User
+from shrapnel.topics.models import Entry
+
+
+class EntrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Entry
+        fields = (
+            "content",
+            "topic",
+            "user",
+            "date_created",
+            "date_updated"
+        )
 
 
 class TopicSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(queryset=User.objects.filter(is_deleted=False), required=True)
+    entries = PresentablePrimaryKeyRelatedField(presentation_serializer=EntrySerializer, many=True, read_only=True)
 
     class Meta:
         model = Topic
@@ -13,7 +29,8 @@ class TopicSerializer(serializers.ModelSerializer):
             "title",
             "user",
             "date_created",
-            "date_updated"
+            "date_updated",
+            "entries"
         )
 
     # TODO: Set user to request.user and make user field read only.
