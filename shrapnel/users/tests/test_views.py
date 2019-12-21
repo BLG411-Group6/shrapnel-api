ac5d625b791718e370e740359eb8b3089c2f9563
baranugur <baranugur7@gmail.com>
2019-12-21 22:42:58 +0300

    Add login & logout tests

---
+++ shrapnel/users/tests/test_views.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from .utils import create_test_user, create_test_user_and_client
+
+
+class LoginViewTests(TestCase):
+    def setUp(self):
+        self.url = reverse('users:login')
+
+        self.username = 'testuser'
+        self.password = 'helloworld'
+
+        self.user = create_test_user({'username': self.username, 'password': self.password})
+        self.client = APIClient()
+
+    def test_successful_login(self):
+        payload = {'username': self.username, 'password': self.password}
+        response = self.client.post(self.url, data=payload)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_unsucessful_login(self):
+        payload = {'username': self.username, 'password': 'wrongpassword'}
+        response = self.client.post(self.url, data=payload)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        import pdb; pdb.set_trace()
+
+
+class LogoutViewTests(TestCase):
+    def setUp(self):
+        self.url = reverse('users:logout')
+        self.user, self.client = create_test_user_and_client()
+
+    def test_logout(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
---
+++ shrapnel/users/tests/utils.py
@@ -0,0 +1,31 @@
+from random import randint
+
+from rest_framework.test import APIClient
+
+from ..models import User
+
+
+def create_test_user(data=None):
+    if data is None:
+        data = dict()
+
+    username = f'test-user-{randint(1, 10000000)}'
+    first_name = 'Test'
+    last_name = 'User'
+    password = 'password'
+
+    data.setdefault('username', username)
+    data.setdefault('first_name', first_name)
+    data.setdefault('last_name', last_name)
+    data.setdefault('email', f'baranugur7+{username}@gmail.com')
+    data.setdefault('password', password)
+
+    return User.objects.create_user(**data)
+
+
+def create_test_user_and_client():
+    user = create_test_user()
+    client = APIClient()
+    client.force_login(user)
+
+    return user, client
