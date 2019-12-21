76c8d6c2a9a339451472aec5f1ff9c5f4d5e8849
Ömer Faruk Abacı <omerfabaci@gmail.com>
2019-12-21 19:15:45 +0300

    Create TopicsView skeleton

--- shrapnel/topics/views.py
+++ shrapnel/topics/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
 
-# Create your views here.
+from shrapnel.topics.serializers import TopicSerializer
+from shrapnel.topics.models import Topic
+
+
+class TopicsView(ListCreateAPIView):
+    permission_classes = []
+    serializer_class = TopicSerializer
+    queryset = Topic.objects.filter(is_deleted=False)
