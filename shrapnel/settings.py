b82a5ac40dc772412f7ce5fc2364f4ff2994e92e
Ömer Faruk Abacı <omerfabaci@gmail.com>
2019-12-22 16:54:06 +0300

    Add Logging settings

--- shrapnel/settings.py
+++ shrapnel/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = 'yo94^370po90vo-eq5lx1n$-j3zmfuc8-m2h%+5y@x&a4g&-9f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = []
 
@@ -140,3 +141,38 @@ MEDIA_URL = '/media/'
 
 # Heroku
 django_heroku.settings(locals())
+
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] '
+                       + 'pathname=%(pathname)s lineno=%(lineno)s '
+                       + 'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
